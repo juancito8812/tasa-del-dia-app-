e95f348208159f3ab9933c90b958ea91fdadc32d
juancito8812 <juanraudel170@gmail.com>
2026-06-11 11:29:59 -0400

    Bump version to 1.0.1

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = {
   expo: {
     name: "Tasa del Día",
     slug: "tasa-del-dia",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
